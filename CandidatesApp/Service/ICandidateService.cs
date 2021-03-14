3c252153c0db68fd8855db47fac465d96209eeb5
Kajaneshan Senthilnathan <mathan_araly@hotmail.com>
2021-03-14 22:07:50 +0000

    Removed unwanted.

--- CandidatesApp/Service/ICandidateService.cs
+++ CandidatesApp/Service/ICandidateService.cs
@@ -5,12 +5,6 @@ namespace CandidatesApp.Service
 {
     public interface ICandidateService
     {
-        ///// <summary>
-        ///// Gets the list of candidates.
-        ///// </summary>
-        ///// <returns>The list of candidates</returns>
-        //IEnumerable<Candidate> GetAllCandidates();
-
         /// <summary>
         /// Gets the list of candidates for a give job title.
         /// </summary>
